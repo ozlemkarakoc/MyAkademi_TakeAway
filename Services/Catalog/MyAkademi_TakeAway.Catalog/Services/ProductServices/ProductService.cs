36a6b5380bb422ab8a91ed2a265846fdcd5c0638
Özlem Karakoç <ozlmkrkoc@gmail.com>
2024-07-12 08:12:31 +0300

    .

---
+++ Services/Catalog/MyAkademi_TakeAway.Catalog/Services/ProductServices/ProductService.cs
@@ -0,0 +1,52 @@
+﻿using AutoMapper;
+using MongoDB.Bson.Serialization;
+using MongoDB.Driver;
+using MyAkademi_TakeAway.Catalog.Dtos.ProductDtos;
+using MyAkademi_TakeAway.Catalog.Entities;
+using MyAkademi_TakeAway.Catalog.Settings;
+
+namespace MyAkademi_TakeAway.Catalog.Services.ProductServices
+{
+    public class ProductService : IProductServices
+    {
+        private readonly IMongoCollection<Product> _productCollection;
+        private readonly IMapper _mapper;
+
+        public ProductService(IMapper mapper, IDatabaseSettings _databaseSettings)
+        {
+            var client = new MongoClient(_databaseSettings.ConnectionString);
+            var database = client.GetDatabase(_databaseSettings.DatabaseName);
+            _productCollection = database.GetCollection<Product>(_databaseSettings.ProductCollectionName);
+            _mapper = mapper;
+        }
+
+        public async Task CreateProductAsync(CreateProductDto createProductDto)
+        {
+            var value = _mapper.Map<Product>(createProductDto);
+            await _productCollection.InsertOneAsync(value);
+        }
+
+        public async Task DeleteProductAsync(string id)
+        {
+            await _productCollection.DeleteOneAsync(x => x.ProductID == id);
+        }
+
+        public async Task<List<ResultProductDto>> GetAllProductAsync()
+        {
+            var values = await _productCollection.Find(x => true).ToListAsync();
+            return _mapper.Map<List<ResultProductDto>>(values);
+        }
+
+        public async Task<GetByIdProductDto> GetByIdProductAsync(string id)
+        {
+            var values = await _productCollection.Find(x => x.ProductID == id).FirstOrDefaultAsync();
+            return _mapper.Map<GetByIdProductDto>(values);
+        }
+
+        public async Task UpdateProductAsync(UpdateProductDto updateProductDto)
+        {
+            var values = _mapper.Map<Product>(updateProductDto);
+            await _productCollection.FindOneAndReplaceAsync(x => x.ProductID == updateProductDto.ProductID, values);
+        }
+    }
+}
---
+++ Services/Catalog/MyAkademi_TakeAway.Catalog/Services/SliderServices/SliderService.cs
@@ -0,0 +1,52 @@
+﻿using AutoMapper;
+using MongoDB.Driver;
+using MyAkademi_TakeAway.Catalog.Dtos.FeatureDtos;
+using MyAkademi_TakeAway.Catalog.Dtos.SliderDtos;
+using MyAkademi_TakeAway.Catalog.Entities;
+using MyAkademi_TakeAway.Catalog.Settings;
+
+namespace MyAkademi_TakeAway.Catalog.Services.SliderServices
+{
+    public class SliderService : ISliderServices
+    {
+        private readonly IMongoCollection<Slider> _sliderCollection;
+        private readonly IMapper _mapper;
+
+        public SliderService(IMapper mapper, IDatabaseSettings _databaseSettings)
+        {
+            var client = new MongoClient(_databaseSettings.ConnectionString);
+            var database = client.GetDatabase(_databaseSettings.DatabaseName);
+            _sliderCollection = database.GetCollection<Slider>(_databaseSettings.FeatureCollectionName);
+            _mapper = mapper;
+        }
+
+        public async Task CreateSliderAsync(CreateSliderDto createSliderDto)
+        {
+            var value = _mapper.Map<Slider>(createSliderDto);
+            await _sliderCollection.InsertOneAsync(value);
+        }
+
+        public async Task DeleteSliderAsync(string id)
+        {
+            await _sliderCollection.DeleteOneAsync(x => x.SliderID == id);
+        }
+
+        public async Task<List<ResultSliderDto>> GetAllSliderAsync()
+        {
+            var values = await _sliderCollection.Find(x => true).ToListAsync();
+            return _mapper.Map<List<ResultSliderDto>>(values);
+        }
+
+        public async Task<GetByIdSliderDto> GetByIdSliderAsync(string id)
+        {
+            var values = await _sliderCollection.Find(x => x.SliderID == id).FirstOrDefaultAsync();
+            return _mapper.Map<GetByIdSliderDto>(values);
+        }
+
+        public async Task UpdateSliderAsync(UpdateSliderDto updateSliderDto)
+        {
+            var values = _mapper.Map<Slider>(updateSliderDto);
+            await _sliderCollection.FindOneAndReplaceAsync(x => x.SliderID == updateSliderDto.SliderID, values);
+        }
+    }
+}
